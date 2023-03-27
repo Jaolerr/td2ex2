ef047328ce64cdcd2b63091f3b815314e2cdaf82
jeremy <jeremy.claude4@gmail.com>
2023-03-27 14:00:24 +0200

    premier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"about",component:AboutComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"accueil",component:AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
